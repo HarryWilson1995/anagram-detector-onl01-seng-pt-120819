ea569abea6ddff6fac9074f25acc5ad001ea20ed
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-09 22:52:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   
   def match(array) 
     array.collect do |potential_anagram|
-      (@word.split("").sort) == (potential_anagramanagram.split("").sort)
+      (@word.split("").sort) == (potential_anagram.split("").sort)
     end 
   end 
   
